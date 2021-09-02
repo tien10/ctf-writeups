b14782b8633880dc3ae610241d90a421ce4b3f03
ph0xen1x0 <77095952+ph0xen1x0@users.noreply.github.com>
2021-09-02 09:51:51 +0700

    upload constraints of checkFlag() function

---
+++ FwordCTF_2021/Omen_chall/checkInput.c
@@ -0,0 +1,99 @@
+flag[33] + (flag[3] ^ (flag[49] ^ flag[78] ^ flag[0])) - flag[49] - flag[26] == 28
+(flag[44] | (flag[69] ^ (flag[43] + flag[21] + (flag[42] & flag[58] & flag[1])))) == -9
+(flag[26] ^ (flag[45] + (flag[64] ^ flag[13] & flag[2]))) - flag[34] - flag[70] == -54
+(flag[76] & (flag[71] + flag[80] + (flag[82] & (flag[40] | (flag[3] - flag[41]))))) == 49
+(flag[30] ^ (flag[14] | ((flag[85] ^ flag[20] ^ flag[35] ^ flag[4]) - flag[64]))) == -93
+flag[70] + flag[55] + (flag[60] ^ (flag[65] + (flag[32] ^ flag[22] ^ flag[5]))) == 94
+flag[18] == (flag[19] ^ (flag[62] ^ flag[74] ^ flag[12] & (flag[6] - flag[6])))
+(flag[3] ^ (flag[81] ^ flag[37] ^ flag[48] ^ (flag[38] + (flag[52] ^ flag[7])))) == -49
+(flag[49] | ((flag[82] & flag[26] & (flag[78] ^ flag[8])) - flag[91])) - flag[53] == -96
+flag[55] + (flag[90] ^ ((flag[63] & (flag[15] ^ flag[9])) - flag[3])) - flag[88] == -57
+(flag[13] | (flag[78] + (flag[84] ^ (flag[72] + (flag[24] ^ (flag[33] | flag[10])))))) == 95 
+(flag[5] | (unsigned __int8)(flag[4] & ((flag[78] ^ flag[11]) - flag[71] - flag[41]))) == 99
+(flag[91] ^ (unsigned __int8)(flag[89] & (flag[92] ^ (flag[1] + (flag[71] & flag[12]) - flag[66])))) == 1
+flag[22] + (flag[32] ^ (unsigned __int8)(flag[21] + flag[38] + (flag[16] ^ flag[13]))) - flag[59] == 13
+(flag[90] | (unsigned __int8)(flag[76] ^ (flag[20] + flag[14] - flag[0]))) - flag[40] - flag[72] == -56
+(flag[76] ^ (unsigned __int8)(flag[76] & (flag[60] + flag[8] + flag[15] - flag[84] - flag[23]))) == 32
+(flag[12] & (unsigned __int8)(flag[90] & (flag[39] ^ flag[82] ^ flag[8] & (flag[5] | flag[16])))) == 32
+(flag[36] ^ (unsigned __int8)(flag[79] | flag[63] | (flag[70] + flag[30] + (flag[43] | flag[17])))) == 57
+(flag[2] ^ (unsigned __int8)(flag[19] | flag[36] ^ (flag[83] | (flag[18] - flag[87])))) - flag[80] == -73
+(flag[64] ^ (unsigned __int8)(flag[6] ^ flag[20] ^ (flag[82] + (flag[10] ^ (flag[19] - flag[63]))))) == -82
+(flag[91] ^ (unsigned __int8)(flag[80] + (flag[13] | flag[39] | flag[71] ^ (flag[57] | flag[20])))) == -55
+(flag[86] ^ (unsigned __int8)(flag[85] ^ flag[86] & (flag[72] ^ (flag[71] + (flag[54] ^ flag[21]))))) == 93
+(flag[57] ^ (unsigned __int8)(flag[19] ^ flag[65] ^ flag[14] ^ (flag[22] - flag[58]))) - flag[38] == 94
+(flag[21] ^ (unsigned __int8)(flag[6] ^ ((flag[26] ^ flag[72] & (flag[73] ^ flag[23])) - flag[1]))) == -12
+(flag[29] & (unsigned __int8)(flag[69] & (flag[36] ^ (flag[52] + (flag[16] & (flag[24] - flag[76])))))) == 4
+(flag[10] | (unsigned __int8)(flag[12] | ((flag[55] & (flag[38] ^ (flag[42] + flag[25]))) - flag[80]))) == -2
+flag[24] + (flag[47] & (unsigned __int8)((flag[16] & (flag[20] + (flag[71] & flag[26]))) - flag[47])) == -125
+(flag[88] & (unsigned __int8)(flag[20] | flag[91] ^ flag[76] & flag[80] & flag[27])) - flag[13] == -11
+flag[12] + (flag[4] & (unsigned __int8)(flag[7] ^ (flag[79] | flag[1] & (flag[89] ^ flag[28])))) == 116
+(flag[95] ^ (unsigned __int8)(flag[32] ^ flag[22] ^ (flag[68] + (flag[58] ^ flag[29]) - flag[54]))) == 76
+(flag[69] ^ (unsigned __int8)(flag[59] ^ flag[26] & (flag[40] | flag[44] ^ flag[41] ^ flag[30]))) == 19
+(flag[50] ^ (unsigned __int8)(flag[28] + (flag[16] & flag[24] & flag[31]) - flag[36] - flag[86])) == -64
+(flag[50] ^ (unsigned __int8)(flag[37] ^ flag[69] ^ flag[54] ^ flag[36] ^ (flag[32] - flag[33]))) == -101
+(flag[56] ^ (unsigned __int8)(flag[12] & (flag[52] ^ (flag[94] + flag[45] + flag[22] + flag[33])))) == 46
+(flag[19] ^ (unsigned __int8)(flag[89] + (flag[6] ^ flag[34]) - flag[23] - flag[20] - flag[42])) == 107
+(flag[10] | (unsigned __int8)(flag[60] + (flag[33] | flag[72] | (flag[27] + (flag[91] ^ flag[35]))))) == 105
+(flag[94] ^ (unsigned __int8)(flag[3] & (flag[24] ^ flag[24] & flag[19] & (flag[42] ^ flag[36])))) == 42
+(flag[17] ^ (unsigned __int8)(flag[57] | flag[71] | ((flag[7] ^ (flag[88] | flag[37])) - flag[19]))) == -114
+(flag[56] ^ (unsigned __int8)(flag[0] ^ flag[76] & (flag[71] ^ flag[76] ^ (flag[38] - flag[9])))) == 57
+(flag[22] ^ (unsigned __int8)(flag[8] ^ (flag[11] + flag[20] + (flag[4] ^ (flag[57] + flag[39]))))) == 12
+flag[91] + (flag[77] | (unsigned __int8)(flag[85] + (flag[38] ^ (flag[67] + flag[40])) - flag[78])) == 47
+flag[84] + (flag[86] ^ (unsigned __int8)(flag[85] ^ flag[51] & (flag[11] ^ (flag[78] | flag[41])))) == -110
+(flag[12] ^ (unsigned __int8)(flag[31] ^ (flag[90] + (flag[27] ^ flag[0] ^ flag[42]) - flag[65]))) == -29
+(flag[75] ^ (unsigned __int8)(flag[71] ^ (flag[64] + (flag[63] ^ flag[40] & (flag[2] ^ flag[43]))))) == 33
+(flag[93] ^ (unsigned __int8)(flag[27] ^ (flag[35] | flag[81] ^ (flag[44] - flag[9])))) - flag[76] == 9
+(flag[88] | (unsigned __int8)(flag[61] & flag[85] & (flag[78] ^ flag[71] & (flag[30] | flag[45])))) == 127
+(flag[24] ^ (unsigned __int8)(flag[41] ^ flag[21] ^ flag[76] ^ ((flag[89] & flag[46]) - flag[90]))) == 66
+(flag[86] ^ (unsigned __int8)(flag[28] + (flag[61] ^ flag[36] ^ flag[47]) - flag[18])) - flag[68] == -4
+(flag[95] ^ (unsigned __int8)(flag[24] + (flag[8] | flag[46] ^ flag[61] ^ (flag[23] | flag[48])))) == -89
+flag[19] + (flag[25] | (unsigned __int8)(flag[95] + (flag[32] ^ flag[67] ^ flag[49]))) - flag[42] == -10
+(flag[67] ^ (unsigned __int8)((flag[90] ^ flag[85] ^ flag[19] ^ flag[50]) - flag[36] - flag[46])) == 97
+(flag[29] ^ (unsigned __int8)(flag[39] ^ flag[4] & ((flag[60] ^ (flag[59] + flag[51])) - flag[56]))) == 67
+!(flag[91] & (unsigned __int8)(flag[36] & (flag[91] + (flag[10] ^ flag[69] ^ (flag[23] + flag[52])))))
+(flag[88] ^ (unsigned __int8)(flag[28] & (flag[19] ^ flag[45] & (flag[52] + (flag[81] ^ flag[53]))))) == 31
+(flag[62] ^ (unsigned __int8)(flag[57] ^ (flag[27] + (flag[44] | ((flag[82] & flag[54]) - flag[60]))))) == -73
+(flag[69] & (unsigned __int8)(flag[12] ^ (flag[10] + (flag[11] ^ flag[81] ^ flag[55]) - flag[17]))) == 94
+flag[88] + (flag[51] ^ (unsigned __int8)((flag[53] ^ flag[19] ^ flag[93] ^ flag[56]) - flag[36])) == 57
+(flag[61] | (unsigned __int8)(flag[69] | flag[16] ^ (flag[13] + (flag[11] ^ flag[18] ^ flag[57])))) == -1
+(flag[89] | (unsigned __int8)(flag[60] ^ flag[61] ^ flag[61] ^ flag[39] ^ flag[67] ^ flag[58])) == 122
+flag[10] + (flag[40] ^ (unsigned __int8)(flag[69] ^ flag[4] & (flag[59] - flag[86] - flag[46]))) == 108
+(flag[40] & (unsigned __int8)(flag[68] | flag[18] ^ ((flag[76] ^ flag[84] ^ flag[60]) - flag[66]))) == 95
+(flag[54] & (unsigned __int8)(flag[53] ^ flag[82] ^ (flag[56] + (flag[81] ^ (flag[61] - flag[87]))))) == 66
+(flag[56] ^ (unsigned __int8)((flag[67] ^ ((flag[41] ^ flag[62]) - flag[42])) - flag[3])) - flag[44] == 120
+(flag[14] ^ (unsigned __int8)(flag[55] | flag[30] ^ (flag[90] | flag[23] | flag[53] ^ flag[63]))) == 11
+(flag[60] & (unsigned __int8)(flag[59] ^ flag[44] ^ flag[46] ^ (flag[71] | flag[29] & flag[64]))) == 34
+(flag[85] | (unsigned __int8)((flag[43] ^ flag[58] & flag[88] & flag[65]) - flag[49] - flag[73])) == -75
+flag[12] + flag[88] + (flag[40] ^ (unsigned __int8)(flag[18] ^ (flag[16] + flag[66] - flag[89]))) == -83
+(flag[59] ^ (unsigned __int8)(flag[90] & ((flag[30] ^ flag[9] ^ flag[3] ^ flag[67]) - flag[48]))) == 127
+(flag[50] & (unsigned __int8)(flag[0] | flag[51] ^ flag[11] ^ flag[36] ^ flag[4] ^ flag[68])) == 86
+(flag[73] ^ (unsigned __int8)(flag[0] ^ ((flag[4] ^ flag[81] ^ flag[55] ^ flag[69]) - flag[19]))) == -77
+(flag[48] ^ (unsigned __int8)(flag[81] ^ flag[12] ^ flag[63] & (flag[15] | flag[37] ^ flag[70]))) == 116
+(flag[51] ^ (unsigned __int8)(flag[57] ^ ((flag[6] ^ flag[77] ^ flag[87] ^ flag[71]) - flag[80]))) == -102
+(flag[12] & (unsigned __int8)(flag[76] ^ flag[57] ^ flag[93] ^ flag[89] & flag[72])) - flag[92] == 6
+(flag[49] ^ (unsigned __int8)(flag[87] + (flag[6] ^ flag[65] ^ flag[94] ^ flag[73]) - flag[56])) == -56
+(flag[10] ^ (unsigned __int8)(flag[35] + (flag[12] | flag[60] ^ flag[74]) - flag[58])) - flag[80] == 72
+(flag[49] ^ (unsigned __int8)(flag[68] ^ (flag[94] + (flag[71] ^ (flag[37] + flag[75] - flag[50]))))) == -96
+(flag[66] ^ (unsigned __int8)((flag[53] | flag[65] ^ flag[15] & (flag[83] + flag[76])) - flag[15])) == 44
+(flag[18] ^ (unsigned __int8)(flag[91] & (flag[30] ^ flag[8] ^ flag[30] ^ (flag[56] + flag[77])))) == 101
+flag[45] + (flag[55] ^ (unsigned __int8)(flag[46] ^ ((flag[58] ^ (flag[79] | flag[78])) - flag[46]))) == -32
+(flag[86] ^ (unsigned __int8)(flag[31] ^ flag[94] ^ flag[24] & (flag[23] | flag[93] | flag[79]))) == 4
+(flag[21] & (unsigned __int8)(flag[7] ^ flag[46] ^ flag[63] & (flag[67] | (flag[80] - flag[55])))) == 64
+(flag[1] | (unsigned __int8)(flag[48] ^ flag[53] ^ flag[44] ^ flag[20] ^ flag[30] & flag[81])) == 119
+(flag[25] | (unsigned __int8)(flag[1] + (flag[23] ^ flag[70] ^ flag[82]) - flag[51] - flag[14])) == 119
+(flag[37] ^ (unsigned __int8)(flag[22] ^ (flag[58] | flag[5] | flag[60] ^ (flag[39] | flag[83])))) == 2
+(flag[51] ^ (unsigned __int8)(flag[51] & ((flag[91] | flag[74] ^ (flag[9] | flag[84])) - flag[6]))) == 42
+(flag[81] ^ (unsigned __int8)(flag[2] ^ flag[19] ^ flag[62] & (flag[47] + (flag[71] ^ flag[85])))) == 20
+(flag[72] ^ (unsigned __int8)(flag[33] & (flag[94] ^ flag[58] ^ flag[70] ^ (flag[84] + flag[86])))) == 2
+(flag[44] | (unsigned __int8)(flag[80] & (flag[88] ^ (flag[20] | flag[36] ^ flag[44] & flag[87])))) == 125
+(flag[81] ^ (unsigned __int8)(flag[38] ^ flag[94] ^ flag[25] & (flag[50] ^ (flag[88] - flag[0])))) == 56
+(flag[3] | (unsigned __int8)(flag[84] | flag[93] ^ (flag[11] | flag[66] & (flag[63] ^ flag[89])))) == 115
+(flag[52] & (unsigned __int8)(flag[13] ^ flag[85] & (flag[16] + (flag[95] ^ (flag[67] | flag[90]))))) == 32
+(flag[44] ^ (unsigned __int8)(flag[68] ^ flag[78] ^ (flag[74] + (flag[80] ^ (flag[51] + flag[91]))))) == 78
+(flag[18] ^ (unsigned __int8)(flag[15] ^ (flag[33] | flag[30] & (flag[87] ^ (flag[92] - flag[59]))))) == 104
+(flag[67] & (unsigned __int8)(flag[39] ^ (flag[76] + (flag[58] ^ flag[15] ^ flag[93]) - flag[6]))) == 4
+(flag[65] & (unsigned __int8)(flag[77] & (flag[66] ^ (flag[0] | flag[72] ^ flag[80] ^ flag[94])))) == 116
+(flag[83] ^ (unsigned __int8)(flag[55] | ((flag[7] ^ flag[70] & (flag[8] ^ flag[95])) - flag[28]))) == -96
+flag[79] == 95
+flag[47] == 52
+flag[61] == 117
